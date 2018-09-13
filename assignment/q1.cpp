b02b9ddb8636c61cec7a938affcbf484fd368c6b
aliakbar09a <aliakbar09a@gmail.com>
2018-09-13 19:30:41 +0530

    q3 completed

--- assignment/q1.cpp
+++ assignment/q1.cpp
@@ -5,17 +5,21 @@ int main()
   char input[1024];
   int table[26] = {0}, i = 0, index;
   // getting the input
-  cout<<"Enter the text\n";
+  cout<<"Enter the text (in lowercase)\n";
   cin.getline(input, 1024);
+  // traversing the input
   while(input[i] != '\0')
   {
     index = input[i] - 97;
+    // checking if input[i] is a lowercase alphabet
     if(index >= 0 || index<=25)
     {
-      table[index]++;
+        // increasing the counter of found letter
+        table[index]++;
     }
     i++;
   }
+  //displaying the counts of each alphabet
   for(i = 0; i<=24; i++)
   {
     cout<<table[i]<<"->\""<<char(i+97)<<"\", ";
--- assignment/q3.cpp
+++ assignment/q3.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 int main()
 {
-    char input[11]= {"11/08/1996"};
-    char* months_list[12] = {"January", "February", "March", "April", "May", "June",
+    char input[11];
+    // list of months
+    const char* months_list[12] = {"January", "February", "March", "April", "May", "June",
                         "July", "August", "September", "October", "November", "December"};
-    int month;
-    cout<<"\nEnter the data in dd/mm/yyyy format\n";
-    // cin.getline(input, 11);
-    cout<<input<<endl;
+    int month, date, year;
+    //getting the input from the user
+    cout<<"Enter the data in dd/mm/yyyy format(without whitespaces) :\t";
+    cin.getline(input, 11);
+    //Displaying the changed format
+    cout<<"\nThe changed format is :\t";
+    // calculating date from string
+    date = 10*(int(input[0]) - int('0')) + (int(input[1]) - int('0'));
+    // calculating month from string
     month = 10*(int(input[3]) - int('0')) + (int(input[4]) - int('0'));
-    cout<<months_list[month - 1]<<endl;
+    // calculating year from string
+    year =1000*(int(input[6]) - int('0')) + 100*(int(input[7]) - int('0'))+
+            10*(int(input[8]) - int('0')) + (int(input[9]) - int('0'));
+    cout<<date<<" "<<months_list[month - 1]<<","<<year<<endl<<endl;
 }
