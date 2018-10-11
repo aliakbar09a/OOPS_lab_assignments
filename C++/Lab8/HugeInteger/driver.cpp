f37a6ac13936f24d0da4173a7d49daac084d7b27
aliakbar09a <aliakbar09a@gmail.com>
2018-10-11 19:27:25 +0530

    Huge Integer completed

--- C++/Lab8/HugeInteger/driver.cpp
+++ C++/Lab8/HugeInteger/driver.cpp
@@ -3,15 +3,77 @@
 using namespace std;
 int main()
 {
-    HugeInteger a;
-    int x;
+    HugeInteger a, b;
+    int x, choice1, choice2;
+    cout<<"\nEnter the first big digit number : ";
     a.input();
-    HugeInteger b;
+    cout<<"\nEnter the second big digit number : ";
     b.input();
-    a.isEqualTo(b);
-    // a.output();
-    // cout<<"\n Enter the number to be subtracted to 40 digit number : ";
-    // cin>>x;
-    // a.subtract(x);
-    // a.output();
+    choice1 = 0;
+    while(choice1 != 4)
+    {
+        cout<<"\nEnter the choice :\n1.Add\n2.Subtract\n3.Compare\n4.Quit\n";
+        cin>>choice1;
+        if(choice1 == 1)
+        {
+            cout<<"\nEnter the choice for addition :\n1.first number\n2.second number\n";
+            cin>>choice2;
+            cout<<"\nEnter the number to be added : ";
+            cin>>x;
+            if(choice2 == 1)
+            {
+                a.add(x);
+                cout<<"\nThe result is : ";
+                a.output();
+            }
+            else if(choice2 == 2)
+            {
+                b.add(x);
+                cout<<"\nThe result is : ";
+                a.output();
+            }
+            else
+                cout<<"\nWrong Choice";
+        }
+        else if(choice1 == 2)
+        {
+            cout<<"\nEnter the choice for subtraction :\n1.first number\n2.second number\n";
+            cin>>choice2;
+            cout<<"\nEnter the number to be subtracted : ";
+            cin>>x;
+            if(choice2 == 1)
+            {
+                a.subtract(x);
+                cout<<"\nThe result is : ";
+                a.output();
+            }
+            else if(choice2 == 2)
+            {
+                b.subtract(x);
+                cout<<"\nThe result is : ";
+                a.output();
+            }
+            else
+                cout<<"\nWrong Choice";
+        }
+        else if(choice1 == 3)
+        {
+            cout<<"\nEnter your choice (first \"choice\" second)\n1.isEqualTo\n2.isNotEqualTo\n3.isGreaterThan\n4.isLessThan\n5.isGreaterThanOrEqualTo\n6.isLessThanOrEqualTo\n";
+            cin>>choice2;
+            if(choice2 == 1)
+                a.isEqualTo(b);
+            else if(choice2 == 2)
+                a.isNotEqualTo(b);
+            else if(choice2 == 3)
+                a.isGreaterThan(b);
+            else if(choice2 == 4)
+                a.isLessThan(b);
+            else if(choice2 == 5)
+                a.isGreaterThanOrEqualTo(b);
+            else if(choice2 == 6)
+                a.isLessThanOrEqualTo(b);
+            else
+                cout<<"\nWrong Choice";
+        }
+    }
 }
